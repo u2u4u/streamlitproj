33a91cbd70d744f47d20d442fefdbe58f63f0650
u2u4u <soheilipour@gmail.com>
2022-12-25 18:14:01 +0330

    ff

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import streamlit as st
+header=st.container()
+with header:
+      st.title('Hello Guys')
+
+
+
+
 f = open("demofile2.txt", "w")
 f.write("Now the file has more content!")
 f.close()
