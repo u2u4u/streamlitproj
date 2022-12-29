a49126d3bdd669c0017598104307299c1fcc2bd8
u2u4u <soheilipour@gmail.com>
2022-12-29 12:05:31 +0330

    sdf

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requirements
+# import requirements
 import streamlit as st
 import telethon
 from telegram import Bot
@@ -34,54 +34,50 @@ st.write('The current movie title is', title)
 
 bot=Bot(requirements.TOKEN)
 bulkchan=-1001861555690
+#***************************Telethon signin part
+# @st.experimental_singleton
+# def sendcode():
+#       loop = asyncio.new_event_loop()
+#       asyncio.set_event_loop(loop)
+#       client=TelegramClient(requirements.SESSION_NAME ,requirements.api_id ,requirements.api_hash, loop=loop)#, loop=loop
+#       st.write(client.is_connected())
+#       client.connect()
+#       st.write("connectiong")
+#       rpl=client.send_code_request(requirements.phone)
+#       print(rpl)
+#       st.write(f"rpl: {rpl.phone_code_hash}")
+#       st.session_state['phash']=rpl.phone_code_hash
+#       st.write(f"rpl: {st.session_state['phash']}")
+#       st.write("sent")
+#       client.disconnect()
+#       return rpl.phone_code_hash
 
-
-
-
-# @st.cache
-@st.experimental_singleton
-def sendcode():
-      loop = asyncio.new_event_loop()
-      asyncio.set_event_loop(loop)
-      client=TelegramClient(requirements.SESSION_NAME ,requirements.api_id ,requirements.api_hash, loop=loop)#, loop=loop
-      st.write(client.is_connected())
-      client.connect()
-      st.write("connectiong")
-      rpl=client.send_code_request(requirements.phone)
-      print(rpl)
-      st.write(f"rpl: {rpl.phone_code_hash}")
-      st.session_state['phash']=rpl.phone_code_hash
-      st.write(f"rpl: {st.session_state['phash']}")
-      st.write("sent")
-      client.disconnect()
-      return rpl.phone_code_hash
-
-
-if st.button('Say hello'):
-      st.write('Why wwwww there')
-      rp=sendcode()
-      print(f"rp:{rp}")
-      print(f"session_state:{st.session_state['phash']}")
-      ee=f"session_state:{st.session_state['phash']}"
-      st.write(st.session_state['phash'])
+# if st.button('Say hello'):
+#       st.write('Why wwwww there')
+#       rp=sendcode()
+#       print(f"rp:{rp}")
+#       print(f"session_state:{st.session_state['phash']}")
+#       ee=f"session_state:{st.session_state['phash']}"
+#       st.write(st.session_state['phash'])
       
-else:
-      st.write('Goodbye')
+# else:
+#       st.write('Goodbye')
 
-if st.button('come in'):
-      if title!='':
-            st.write('sdf')
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            client=TelegramClient(requirements.SESSION_NAME ,requirements.api_id ,requirements.api_hash, loop=loop)#, loop=loop
-            st.write(client.is_connected())
-            client.connect()
-            st.write("connectiong")
-            st.write(title)
-            st.write(st.session_state['phash'])
-            client.sign_in(phone=requirements.phone, code=title,phone_code_hash=st.session_state['phash'])
-else:
-      st.write('Goodbye')
+# if st.button('come in'):
+#       if title!='':
+#             st.write('sdf')
+#             loop = asyncio.new_event_loop()
+#             asyncio.set_event_loop(loop)
+#             client=TelegramClient(requirements.SESSION_NAME ,requirements.api_id ,requirements.api_hash, loop=loop)#, loop=loop
+#             st.write(client.is_connected())
+#             client.connect()
+#             st.write("connectiong")
+#             st.write(title)
+#             st.write(st.session_state['phash'])
+#             client.sign_in(phone=requirements.phone, code=title,phone_code_hash=st.session_state['phash'])
+# else:
+#       st.write('Goodbye')
+#EOF***************************Telethon signin part
 @st.cache(ttl = 30)
 def sendmes():
       res=bot.send_message(bulkchan,"STREAMLIT")
